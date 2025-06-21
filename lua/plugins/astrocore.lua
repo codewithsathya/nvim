94586a9beb917af434571a9ff6ee65a5fac30e8b
Sathiya Sivaraj <codewithsathya@gmail.com>
2025-06-21 14:50:53 +0530

    fix auto complete

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -4,24 +4,27 @@ return {
     ---@type AstroCoreOpts
     opts = {
         features = {
-            large_buf = { size = 1024 * 256, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
-            autopairs = true, -- enable autopairs at start
-            cmp = true, -- enable completion at start
-            diagnostics = { virtual_text = false, virtual_lines = false }, -- diagnostic settings on startup
-            highlighturl = true, -- highlight URLs at start
-            notifications = false, -- enable notifications at start
+            large_buf = { size = 1024 * 256, lines = 10000 },
+            autopairs = true,
+            cmp = true,
+            diagnostics = { virtual_text = false, virtual_lines = false },
+            highlighturl = true,
+            notifications = false
         },
         diagnostics = {
             virtual_text = false,
             underline = true,
         },
         options = {
-            opt = { -- vim.opt.<key>
-                relativenumber = true, -- sets vim.opt.relativenumber
-                number = true, -- sets vim.opt.number
-                spell = false, -- sets vim.opt.spell
-                signcolumn = "yes", -- sets vim.opt.signcolumn to yes
-                wrap = false, -- sets vim.opt.wrap
+            opt = {
+                relativenumber = true,
+                number = true,
+                spell = false,
+                signcolumn = "yes",
+                wrap = false,
+                tabstop = 4,
+                shiftwidth = 4,
+                expandtab = true,
             },
         },
     },
